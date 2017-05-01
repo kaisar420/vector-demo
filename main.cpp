4035005b1ef2df96a35df1d23b21cc63e0bb8766
Ivan Sorokin <vanyacpp@gmail.com>
2017-05-01 15:07:23 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,394 @@
+#include "vector.h"
+#include "gtest/gtest.h"
+
+template struct vector<int>;
+
+template <typename T>
+T const& as_const(T& obj)
+{
+    return obj;
+}
+
+template <typename T>
+struct counted
+{
+    counted()
+    {
+        ++instances();
+    }
+
+    counted(T const& val)
+        : val(val)
+    {
+        ++instances();
+    }
+
+    counted(counted const& rhs)
+        : val(rhs.val)
+    {
+        copy();
+        ++instances();
+    }
+
+    counted& operator=(counted const& rhs)
+    {
+        copy();
+        val = rhs.val;
+        return *this;
+    }
+
+    ~counted()
+    {
+        --instances();
+    }
+
+    static size_t& instances()
+    {
+        static size_t value = 0;
+        return value;
+    }
+
+    static void expect_no_instances()
+    {
+        EXPECT_EQ(0, instances());
+    }
+
+    static void set_throw_countdown(size_t val)
+    {
+        throw_countdown = val;
+    }
+
+    friend bool operator==(counted const& a, counted const& b)
+    {
+        return a.val == b.val;
+    }
+
+    friend bool operator!=(counted const& a, counted const& b)
+    {
+        return a.val != b.val;
+    }
+
+private:
+    void copy()
+    {
+        if (throw_countdown != 0)
+        {
+            --throw_countdown;
+            if (throw_countdown == 0)
+                throw std::runtime_error("copy failed");
+        }
+    }
+
+private:
+    T val;
+    static size_t throw_countdown;
+};
+
+template <typename T>
+size_t counted<T>::throw_countdown = 0;
+
+TEST(correctness, default_ctor)
+{
+    vector<counted<int> > a;
+    counted<int>::expect_no_instances();
+    EXPECT_TRUE(a.empty());
+    EXPECT_EQ(0, a.size());
+}
+
+TEST(correctness, push_back)
+{
+    {
+        vector<counted<size_t> > a;
+        for (size_t i = 0; i != 200; ++i)
+            a.push_back(i);
+
+        for (size_t i = 0; i != 200; ++i)
+            EXPECT_EQ(i, a[i]);
+    }
+
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, subscription)
+{
+    vector<int> a;
+    a.push_back(4);
+    a.push_back(8);
+    a.push_back(15);
+    a.push_back(16);
+    a.push_back(23);
+    a.push_back(42);
+
+    EXPECT_EQ(4, a[0]);
+    EXPECT_EQ(8, a[1]);
+    EXPECT_EQ(15, a[2]);
+    EXPECT_EQ(16, a[3]);
+    EXPECT_EQ(23, a[4]);
+    EXPECT_EQ(42, a[5]);
+
+    vector<int> const& ca = a;
+    EXPECT_EQ(4, ca[0]);
+    EXPECT_EQ(8, ca[1]);
+    EXPECT_EQ(15, ca[2]);
+    EXPECT_EQ(16, ca[3]);
+    EXPECT_EQ(23, ca[4]);
+    EXPECT_EQ(42, ca[5]);
+}
+
+TEST(correctness, data)
+{
+    vector<counted<size_t> > a;
+    a.push_back(5);
+    a.push_back(6);
+    a.push_back(7);
+    
+    {
+        counted<size_t>* ptr = a.data();
+        EXPECT_EQ(5, ptr[0]);
+        EXPECT_EQ(6, ptr[1]);
+        EXPECT_EQ(7, ptr[2]);
+    }
+
+    {
+        counted<size_t> const* cptr = as_const(a).data();
+        EXPECT_EQ(5, cptr[0]);
+        EXPECT_EQ(6, cptr[1]);
+        EXPECT_EQ(7, cptr[2]);
+    }
+}
+
+TEST(correctness, front_back)
+{
+    vector<counted<size_t> > a;
+    a.push_back(5);
+    a.push_back(6);
+    a.push_back(7);
+
+    EXPECT_EQ(5, a.front());
+    EXPECT_EQ(5, as_const(a).front());
+
+    EXPECT_EQ(7, a.back());
+    EXPECT_EQ(7, as_const(a).back());
+}
+
+TEST(correctness, capacity)
+{
+    {
+        vector<counted<size_t> > a;
+        a.reserve(10);
+        EXPECT_GE(a.capacity(), 10);
+        a.push_back(5);
+        a.push_back(6);
+        a.push_back(7);
+        EXPECT_GE(a.capacity(), 10);
+        a.shrink_to_fit();
+        EXPECT_EQ(3, a.capacity());
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, superfluous_reserve)
+{
+    {
+        vector<counted<size_t> > a;
+        a.reserve(10);
+        size_t c = a.capacity(); 
+        EXPECT_GE(c, 10);
+        a.reserve(5);
+        EXPECT_GE(a.capacity(), 10);
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, clear)
+{
+    {
+        vector<counted<size_t> > a;
+        a.push_back(5);
+        a.push_back(6);
+        a.push_back(7);
+        size_t c = a.capacity(); 
+        a.clear();
+        EXPECT_EQ(c, a.capacity());
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, superfluous_shrink_to_fit)
+{
+    {
+        vector<counted<size_t> > a;
+        a.reserve(10);
+        size_t n = a.capacity(); 
+        for (size_t i = 0; i != n; ++i)
+            a.push_back(i);
+
+        counted<size_t>* old_data = a.data();        
+        a.shrink_to_fit();
+        
+        EXPECT_EQ(old_data, a.data());
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, copy_ctor)
+{
+    {
+        size_t const N = 5;
+        vector<counted<size_t> > a;
+        for (size_t i = 0; i != N; ++i)
+            a.push_back(i);
+
+        vector<counted<size_t> > b = a;
+        for (size_t i = 0; i != N; ++i)
+            EXPECT_EQ(i, b[i]);
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, assignment_operator)
+{
+    {
+        size_t const N = 5;
+        vector<counted<size_t> > a;
+        for (size_t i = 0; i != N; ++i)
+            a.push_back(i);
+
+        vector<counted<size_t> > b;
+        b.push_back(42);
+
+        b = a;
+        EXPECT_EQ(N, b.size());
+        for (size_t i = 0; i != N; ++i)
+            EXPECT_EQ(i, b[i]);
+
+        b.push_back(5);
+        EXPECT_EQ(5, b[5]);
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, self_assignment)
+{
+    {
+        vector<counted<size_t> > a;
+        a.push_back(5);
+        a.push_back(6);
+        a.push_back(7);
+
+        a = a;
+
+        EXPECT_EQ(5, a[0]);
+        EXPECT_EQ(6, a[1]);
+        EXPECT_EQ(7, a[2]);
+    }
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, pop_back)
+{
+    vector<counted<size_t> > a;
+    a.push_back(5);
+    a.push_back(6);
+    a.push_back(7);
+
+    EXPECT_EQ(7, a.back());
+    a.pop_back();
+    EXPECT_EQ(2, a.size());
+
+    EXPECT_EQ(6, a.back());
+    a.pop_back();
+    EXPECT_EQ(1, a.size());
+
+    EXPECT_EQ(5, a.back());
+    a.pop_back();
+    EXPECT_EQ(0, a.size());
+
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, empty)
+{
+    vector<counted<size_t> > a;
+
+    EXPECT_TRUE(a.empty());
+    a.push_back(5);
+    EXPECT_FALSE(a.empty());
+    a.pop_back();
+    EXPECT_TRUE(a.empty());
+}
+
+TEST(correctness, insert_begin)
+{
+    size_t const N = 100;
+    vector<counted<size_t> > a;
+
+    for (size_t i = 0; i != N; ++i)
+        a.insert(a.begin(), i);
+
+    for (size_t i = 0; i != N; ++i)
+    {
+        EXPECT_EQ(i, a.back());
+        a.pop_back();
+    }
+}
+
+TEST(correctness, insert_end)
+{
+    {
+        vector<counted<size_t> > a;
+        
+        a.push_back(4);
+        a.push_back(5);
+        a.push_back(6);
+        a.push_back(7);
+    
+        EXPECT_EQ(4, a.size());
+    
+        a.insert(a.end(), 8);
+        EXPECT_EQ(5, a.size());
+        EXPECT_EQ(8, a.back());
+    
+        a.insert(a.end(), 9);
+        EXPECT_EQ(6, a.size());
+        EXPECT_EQ(9, a.back());
+    }
+    
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, erase)
+{
+    {
+        vector<counted<size_t> > a;
+        
+        a.push_back(4);
+        a.push_back(5);
+        a.push_back(6);
+        a.push_back(7);
+
+        a.erase(a.begin() + 2);
+
+        EXPECT_EQ(3, a.size());
+        EXPECT_EQ(4, a[0]);
+        EXPECT_EQ(5, a[1]);
+        EXPECT_EQ(7, a[2]);
+    }
+    
+    counted<size_t>::expect_no_instances();
+}
+
+TEST(correctness, reallocation_throw)
+{
+    {
+        vector<counted<size_t> > a;
+        a.reserve(10);
+        size_t n = a.capacity();
+        for (size_t i = 0; i != n; ++i)
+            a.push_back(i);
+        
+        counted<size_t>::set_throw_countdown(7);
+        EXPECT_THROW(a.push_back(42), std::runtime_error);
+    }
+    counted<size_t>::expect_no_instances();
+}
---
+++ vector.h
@@ -0,0 +1,418 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <new>
+#include <type_traits>
+#include <utility>
+
+template <typename TT>
+void destroy_all(TT* data, size_t size,
+    typename std::enable_if<!std::is_trivially_destructible<TT>::value>::type* = nullptr)
+{
+    for (size_t i = 0; i != size; i++)
+        data[i].~TT();
+}
+
+template <typename TT>
+void destroy_all(TT*, size_t,
+    typename std::enable_if<std::is_trivially_destructible<TT>::value>::type* = nullptr)
+{}
+
+template <typename TT>
+void copy_construct_all(TT* dst, TT const* src, size_t size,
+    typename std::enable_if<!std::is_trivially_copyable<TT>::value>::type* = nullptr)
+{
+    size_t i = 0;
+    
+    try
+    {
+        for (; i != size; ++i)
+            new (dst + i) TT(src[i]);
+    }
+    catch (...)
+    {
+        while (i != 0)
+        {
+            dst[i - 1].~TT();
+            --i;
+        }
+        throw;
+    }
+}
+
+template <typename TT>
+void copy_construct_all(TT* dst, TT const* src, size_t size,
+    typename std::enable_if<std::is_trivially_copyable<TT>::value>::type* = nullptr)
+{
+    memcpy(dst, src, size * sizeof(TT));
+}
+
+template <typename T>
+struct vector
+{
+    typedef T* iterator;
+    typedef T const* const_iterator;
+
+    vector();
+    vector(vector const&);
+    vector& operator=(vector const& other);
+
+    ~vector();
+
+    T& operator[](size_t i);
+    T const& operator[](size_t i) const;
+
+    T* data();
+    T const* data() const;
+
+    size_t size() const;
+
+    T& front();
+    T const& front() const;
+
+    T& back();
+    T const& back() const;
+
+    bool empty() const;
+
+    size_t capacity() const;
+    void reserve(size_t);
+    void shrink_to_fit();
+    
+    void clear();
+    
+    void push_back(T const&);
+    void pop_back();
+    
+    void swap(vector&);
+
+    iterator insert(iterator pos, T const&);
+    iterator insert(const_iterator pos, T const&);
+
+    iterator erase(iterator pos);
+    iterator erase(const_iterator pos);
+
+    iterator erase(iterator first, iterator last);
+    iterator erase(const_iterator first, const_iterator last);
+
+    iterator begin();
+    iterator end();
+
+    const_iterator begin() const;
+    const_iterator end() const;
+
+private:
+    size_t increase_capacity() const;
+    void push_back_realloc(T const&);
+    void new_buffer(size_t new_capacity);
+    
+private:
+    T* data_;
+    size_t size_;
+    size_t capacity_;
+};
+
+template <typename T>
+vector<T>::vector()
+    : data_(nullptr)
+    , size_(0)
+    , capacity_(0)
+{}
+
+template <typename T>
+vector<T>::vector(vector const& other)
+    : vector()
+{
+    new_buffer(other.size());
+    copy_construct_all(data_, other.data_, other.size_);
+    size_ = other.size_;
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(vector const& other)
+{
+    vector copy(other);
+    swap(copy);
+    return *this;
+}
+
+template <typename T>
+vector<T>::~vector()
+{
+    destroy_all(data_, size_);
+    operator delete(data_);
+}
+
+template <typename T>
+T& vector<T>::operator[](size_t i)
+{
+    return data_[i];
+}
+
+template <typename T>
+T const& vector<T>::operator[](size_t i) const
+{
+    return data_[i];
+}
+
+template <typename T>
+T* vector<T>::data()
+{
+    return data_;
+}
+
+template <typename T>
+T const* vector<T>::data() const
+{
+    return data_;
+}
+
+template <typename T>
+size_t vector<T>::size() const
+{
+    return size_;    
+}
+
+template <typename T>
+T& vector<T>::front()
+{
+    return *data_;
+}
+
+template <typename T>
+T const& vector<T>::front() const
+{
+    return *data_;
+}
+
+
+template <typename T>
+T& vector<T>::back()
+{
+    return data_[size_ - 1];
+}
+
+template <typename T>
+T const& vector<T>::back() const
+{
+    return data_[size_ - 1];
+}
+
+template <typename T>
+bool vector<T>::empty() const
+{
+    return size_ == 0;
+}
+
+template <typename T>
+size_t vector<T>::capacity() const
+{
+    return capacity_;
+}
+
+template <typename T>
+void vector<T>::reserve(size_t desired_capacity)
+{
+    if (desired_capacity < capacity_)
+        return;
+
+    new_buffer(desired_capacity);
+}
+
+template <typename T>
+void vector<T>::shrink_to_fit()
+{
+    if (capacity_ == size_)
+        return;
+
+    new_buffer(size_);
+}
+
+template <typename T>
+void vector<T>::clear()
+{
+    destroy_all(data_, size_);
+    size_ = 0;
+}
+
+template <typename T>
+void vector<T>::push_back(T const& val)
+{
+    if (size_ != capacity_)
+    {
+        new (data_ + size_) T(val);
+        ++size_;
+    }
+    else
+    {
+        push_back_realloc(val);
+    }
+}
+
+template <typename T>
+void vector<T>::pop_back()
+{
+    assert(size_ != 0);
+
+    data_[size_ - 1].~T();
+    --size_;
+}
+
+template <typename T>
+void vector<T>::swap(vector& other)
+{
+    using std::swap;
+
+    swap(data_,     other.data_);
+    swap(size_,     other.size_);
+    swap(capacity_, other.capacity_);
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::insert(iterator pos, T const& val)
+{
+    if (size_ == capacity_)
+    {
+        vector tmp;
+        tmp.new_buffer(increase_capacity());
+        copy_construct_all(tmp.data_, data_, pos - begin());
+        tmp.size_ = pos - begin();
+        
+        auto result = tmp.end();
+
+        tmp.push_back(val);
+        
+        copy_construct_all(tmp.end(), pos, end() - pos);
+        tmp.size_ += end() - pos;
+        
+        swap(tmp);
+        return result;
+    }
+    
+    push_back(back());
+
+    for (auto i = (end() - 1); i != pos; --i)
+    {
+        using std::swap;
+        swap(*i, *(i - 1));
+    }
+    
+    *pos = val;
+    return pos;
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::insert(const_iterator pos, T const& val)
+{
+    return insert(data_ + (pos - data_), val);
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::erase(iterator pos)
+{
+    return erase(pos, pos + 1);
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::erase(const_iterator pos)
+{
+    return erase(data_ + (pos - data_));
+    
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::erase(iterator first, iterator last)
+{
+    iterator result = first;
+
+    while (last != end())
+    {
+        using std::swap;
+        swap(*first, *last);
+        ++first;
+        ++last;
+    }
+
+    size_ = first - data_;
+    while (first != last)
+    {
+        first->~T();
+        ++first;
+    }
+
+    return result;
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::erase(const_iterator first, const_iterator last)
+{
+    return erase(data_ + (first - data_),
+                 data_ + (last  - data_));
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::begin()
+{
+    return data_;
+}
+
+template <typename T>
+typename vector<T>::iterator vector<T>::end()
+{
+    return data_ + size_;
+}
+
+
+template <typename T>
+typename vector<T>::const_iterator vector<T>::begin() const
+{
+    return data_;
+}
+
+
+template <typename T>
+typename vector<T>::const_iterator vector<T>::end() const
+{
+    return data_ + size_;
+}
+
+
+template <typename T>
+size_t vector<T>::increase_capacity() const
+{
+    if (capacity_ == 0)
+        return 4;
+    else
+        return capacity_ * 3 / 2;
+}
+
+template <typename T>
+void vector<T>::push_back_realloc(T const& val)
+{
+    vector<T> tmp;
+    tmp.new_buffer(increase_capacity());
+    copy_construct_all(tmp.data_, data_, size_);
+    tmp.size_ = size_;
+
+    tmp.push_back(val);
+    swap(tmp);
+}
+
+template <typename T>
+void vector<T>::new_buffer(size_t new_capacity)
+{
+    assert(new_capacity >= size_);
+
+    vector<T> tmp;
+    tmp.data_ = static_cast<T*>(operator new(new_capacity * sizeof(T)));
+    tmp.capacity_ = new_capacity;
+    copy_construct_all(tmp.data_, data_, size_);
+    tmp.size_ = size_;
+
+    swap(tmp);
+}
+
+#endif // VECTOR_H
